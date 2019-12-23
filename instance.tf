30a40802ff9ce2af5e6cd8981c49d134f0ff31d5
ktalant <uchkunfan@gmail.com>
2019-12-23 14:36:12 -0500

    security group is added

--- instance.tf
+++ instance.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "dev_instance" {
-  ami           = var.vm_ami
-  instance_type = var.vm_type
+  ami                   = var.vm_ami
+  instance_type         = var.vm_type
+  security_groups       = aws_security_group.my_dev_sg
 }
