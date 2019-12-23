5c61b6c4c670f9511e954bda308a401a3369c4fd
ktalant <uchkunfan@gmail.com>
2019-12-22 23:44:40 -0500

    security group has been updated

--- my_sec_group.tf
+++ my_sec_group.tf
@@ -1,20 +1,39 @@
 
 resource "aws_security_group" "my_dev_sg" {
   name        = var.var_my_dev_sg
-  description = "Allow ssh http and https inbound traffic"
+  description = "Allow TLS inbound traffic"
   vpc_id      = "vpc-be96e0d6"
+
+  ingress {
+    # http (change to whatever ports you need)
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     # TLS (change to whatever ports you need)
     from_port   = 443
-    from_port   = 80
-    to_port     = 80
     to_port     = 443
     protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    # ssh (change to whatever ports you need)
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   egress {
     from_port       = 0
