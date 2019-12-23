b5effbdc52560e63c12f3d6a90742950d31ff555
ktalant <uchkunfan@gmail.com>
2019-12-22 22:48:00 -0500

    key pair has been updated

--- my_key_pair.tf
+++ my_key_pair.tf
@@ -1,5 +1,4 @@
 resource "aws_key_pair" "talant_key_pair" {
   key_name              = var.var_key
-  public_key            = var.var_public_key
-  
+  public_key            = file("~/.ssh/id_rsa.pub")
 }
