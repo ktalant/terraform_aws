c7e5e9d53da47e01de76d53d1ea7b22b2d6ff5a3
ktalant <uchkunfan@gmail.com>
2019-12-24 10:29:46 -0500

    count indiced

--- my_output.tf
+++ my_output.tf
@@ -1,10 +1,10 @@
 output "dev_public_ip" {
-  value = aws_instance.dev_instance.public_ip
+  value = aws_instance.dev_instance[0].public_ip
 }
 output "dev_ami_id" {
-  value = aws_instance.dev_instance.ami
+  value = aws_instance.dev_instance[0].ami
 }
 
 output "dev_sec_group" {
-  value = aws_security_group.sg2.name
+  value = aws_security_group.sg2[0].name
 }
