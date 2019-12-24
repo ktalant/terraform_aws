f83318186588007051f0cf1bca24476572161c1b
ktalant <uchkunfan@gmail.com>
2019-12-24 10:25:24 -0500

    instance updated

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "dev_instance" {
   ami                   = var.vm_ami
   instance_type         = var.vm_type
-  security_groups       =  var.var_new_sg
+  security_groups       = var.var_new_sg
   count                 = var.vm_count
   
   tags = {
